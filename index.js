37e5efa00948d028b16b5b3456727767f567eb8b
blitzparanoia <bluemilk44@gmail.com>
2022-03-27 12:33:31 -0400

    added loop that displays the list, corrected typo

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 let input = prompt('what would you like  to do?');
-const todos= [thing one, thing two];
+const todos= ['thing one', 'thing two'];
 
 while (input !== 'quit' && input !== 'q') {
-    if
-    input = prompt ('what would yopu like to do?')
+    if (input === 'list') {
+        console.log('***************')
+        //print out the array and the index using a for loop
+        for(let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('***************')
+    }
+    input = prompt ('what would you like to do?')
 }
 console.log('OK, you quit the app')
