3ff932729e9b2be894f8a1b7e9e20df9dd2ecbe2
blitzparanoia <bluemilk44@gmail.com>
2022-03-27 13:15:15 -0400

    delete feature does not accept letters

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ while (input !== 'quit' && input !== 'q') {
         todos.push(newTodo);
         console.log(`${newTodo} added to the list.`)
     } else if (input == 'delete'){
-        const index = prompt('Enter an index to delete:')
+        const index = parseInt(prompt('Enter an index to delete:'));
+        if (!Number.isNaN(index)) {
         const deleted = todos.splice(index, 1);
         console.log(`You have deleted ${deleted[0]}`);
-        //use splice method to remove the index
+        } else {
+            console.log(`Unknown index`)
+        }
     }
     input = prompt ('what would you like to do?')
 }
