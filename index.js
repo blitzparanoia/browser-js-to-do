3e70777930968df3f875e838d8870bcc12e697db
blitzparanoia <bluemilk44@gmail.com>
2022-03-26 20:00:54 -0400

    added input cariable and list holder, created loop for app quit

---
+++ index.js
@@ -0,0 +1,8 @@
+let input = prompt('what would you like  to do?');
+const todos= [thing one, thing two];
+
+while (input !== 'quit' && input !== 'q') {
+    if
+    input = prompt ('what would yopu like to do?')
+}
+console.log('OK, you quit the app')
